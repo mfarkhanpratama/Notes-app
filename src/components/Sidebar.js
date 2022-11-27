6ddcfbd175f964c72469233a45cf83a203eca8d3
m fakrhan pratama <farkhan3123@gmail.com>
2022-11-27 21:55:26 +0700

    adding searchbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactMarkdown from 'react-markdown'
 
 function Sidebar({ notes , onAddNote ,onDeleteNote ,activeNote, setActiveNote}){
 
+    const [search,setSearch] = useState('');
+
     const sortedNotes = notes.sort((a,b)=> b.lastModified - a.lastModified );
+
+    const onSearch = (e) =>{
+        setSearch(e.target.value);
+    }
+
+    const searchNotes = notes.filter((note)=>{
+        return search && note.title.toLowerCase().startsWith(search.toLowerCase())
+    })
 
     return (
         <div className="app-sidebar">
@@ -11,10 +21,14 @@ function Sidebar({ notes , onAddNote ,onDeleteNote ,activeNote, setActiveNote}){
                 <h1>Notes</h1>
                 <button onClick={onAddNote}>Add</button>
             </div>
+            <div className="app-sidebar-search">
+                <input type="text" placeholder="search..." value={search} onChange={onSearch}/>
+            </div>
             <div className="app-sidebar-notes">
-                {sortedNotes.map((note)=>(
+                {console.log(searchNotes)}
+                {(search? searchNotes : sortedNotes).map((note)=>(
                     <div className={`app-sidebar-note ${note.id === activeNote && 'active'}`} onClick={()=>setActiveNote(note.id) } key={note.id}>
-                        <div className="sidebar-note-tittle">
+                        <div className="sidebar-note-title">
                             <strong>{note.title}</strong>
                             <button onClick={()=>onDeleteNote(note.id)}>Delete</button>
                         </div>
